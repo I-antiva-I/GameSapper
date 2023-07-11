10cf930cfff71ec95c4c295f1a413d02ed4c154d
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-07-12 00:20:41 +0200

    Other

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+import App from './components/App';
+
+import './css/Basic.css';
+import './css/App.css';
+// COMPONENTS' CSS
+import "./css/GameField.css"
+import "./css/Options.css"
+import "./css/GameTitle.css"
+import "./css/GameInfo.css"
+// FUNCTIONAL
+import "./css/Functional.css"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render
+(
+<App/>
+);
+
