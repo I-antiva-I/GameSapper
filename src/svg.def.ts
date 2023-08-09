e69ba8a408a379349463205225a64d7889515498
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-08-09 23:31:26 +0200

    Support for .SVG files

---
+++ src/svg.def.ts
@@ -0,0 +1,8 @@
+declare module "*.svg" 
+{
+    //const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+    const content: string;
+    export default content;
+}
+
+// https://stackoverflow.com/questions/44717164/unable-to-import-svg-files-in-typescript
