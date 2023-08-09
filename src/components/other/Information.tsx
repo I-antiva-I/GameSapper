a7f302afc1a94fedec68e2f51d648ddc3d4d335d
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-08-09 05:14:19 +0200

    Temporary placeholders

---
+++ src/components/other/Information.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Information()
+{
+    return(
+        <div className="information">
+            <h2>INFO PLACEHOLDER</h2>
+        </div>
+    )
+}
+
+
+export default Information;
---
+++ src/components/other/ScoreTable.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function ScoreTable()
+{
+    return(
+        <div className="score-table">
+            <h2>SCORE PLACEHOLDER</h2>
+        </div>
+    )
+}
+
+
+export default ScoreTable;
