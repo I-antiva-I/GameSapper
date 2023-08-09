ad723f43ea7bc0efbbaa7918bf69ea9124917054
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-08-09 04:03:35 +0200

    Created index.tsx, Added StrictMode

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from "./components/App";
+
+import "./css/styles_variables.css"
+import "./css/styles_main.css"
+import "./css/styles_game.css"
+import "./css/styles_control.css"
+import "./css/styles_other.css"
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
