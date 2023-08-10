8edcc6f583ba03f032fed28b096a44b56b72d9c9
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-08-10 03:06:11 +0200

    Comments removed

--- src/components/other/Timer.tsx
+++ src/components/other/Timer.tsx
@@ -18,40 +18,6 @@ function Timer()
         return () => clearInterval(intervalId);
       }, [isActive, timeSeconds]);
 
-
-    /*
-    useEffect(
-        () => 
-        {
-            props.functionGetTimeVal.current = () =>
-            {
-                return 65757;
-            }
-
-            props.functionResetTimeVal.current = () =>
-            {
-                setTimeSeconds(0);
-            }
-
-        },[])
-
-    useEffect(
-        () => 
-        {
-            if(props.state)
-            {
-                timeInterval.current = setInterval(() => 
-                    {setTimeSeconds(prevTS => {return prevTS+1})} ,1000);
-            }
-            else
-            {   
-                clearInterval(timeInterval.current);
-            }
-            
-        },[props.state])
-
-    */
-
     let m = (Math.floor(timeSeconds/60)).toString().padStart(2,"0");
     let s = (timeSeconds%60).toString().padStart(2,"0");
     
