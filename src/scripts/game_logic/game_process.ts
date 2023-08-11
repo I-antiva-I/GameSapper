7058be77f57799ff8c0312d3fd0e3883e028f9d6
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-08-11 04:02:45 +0200

    GameResult and GameState expanded,

--- src/scripts/game_logic/game_process.ts
+++ src/scripts/game_logic/game_process.ts
@@ -3,26 +3,51 @@ import { LogicField } from "./logic_field";
 class GameState 
 {
     phase:          GamePhase;
-    logicField:     LogicField
+    logicField:     LogicField;
+    victory:        boolean;
 
-    constructor(phase: GamePhase, logicField: LogicField)
+    constructor(phase: GamePhase, logicField: LogicField, victory: boolean)
     {
         this.phase  = phase;
         this.logicField = logicField;
+        this.victory = victory;
     }
 }
 
 class GameResult
 {
-    numberOfBombs:  number;
-    numberOfCells:  number;
-    totalTime:      number;
+    victory:            boolean;
+    numberOfBombs:      number;
+    numberOfRows:       number;
+    numberOfColumns:    number;
+    time:               number;
+    date:               Date;
 
-    constructor(numberOfBombs: number, numberOfCells: number, totalTime: number)
+    constructor(victory: boolean, numberOfBombs: number, numberOfRows: number, numberOfColumns: number, time: number, date: Date)
     {
-        this.numberOfBombs  = numberOfBombs;
-        this.numberOfCells = numberOfCells;
-        this.totalTime = totalTime;
+        this.victory =          victory;
+        this.numberOfBombs  =   numberOfBombs;
+        this.numberOfRows =     numberOfRows;
+        this.numberOfColumns =  numberOfColumns;
+        this.time =             time;
+        this.date =             date;
+    }
+
+    isGreaterThan(object: GameResult) : boolean
+    {
+        if      ((this.victory !== object.victory)) 
+                {return this.victory;}
+
+        else if (this.numberOfBombs !== object.numberOfBombs)
+                {return (this.numberOfBombs>object.numberOfBombs);}
+
+        else if ((this.numberOfRows*this.numberOfColumns) !== (object.numberOfRows*object.numberOfColumns)) 
+                {return (this.numberOfRows*this.numberOfColumns) > (object.numberOfRows*object.numberOfColumns);}
+
+        else if (this.time !== object.time)
+                {return (this.time<object.time);}
+
+        else    {return (this.date >= object.date);}
     }
 }
 
