b1023181cd16e019ec52762033c23349214d46d9
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-08-15 21:23:04 +0200

    Information about score

--- src/components/score/ScoreResult.tsx
+++ src/components/score/ScoreResult.tsx
@@ -1,24 +1,38 @@
 import React from "react";
 import { GameResult } from "../../scripts/game_logic/game_process";
 
-
 interface ScoreResultProps
 {
     result: GameResult,
 }
 
-
 function ScoreResult(props: ScoreResultProps)
 {   
     return(
-        <div className="result-content">
-            <div className="result-value">{props.result.numberOfBombs}</div>
-            <div className="result-value">{props.result.numberOfColumns}</div>
-            <div className="result-value">{props.result.numberOfRows}</div>
-            <div className="result-value">{props.result.time}</div>
+        <div className="score-result-content">
+            <div className={"score-result-value result-"+((props.result.victory) ? "victory" : "defeat")}>
+                {
+                    (props.result.victory) ? "Victory" : "Defeat"
+                }
+            </div>
+
+            <div className="score-result-icon">💣</div>
+            <div className="score-result-label">Number Of Bombs</div>
+            <div className="score-result-value">{props.result.numberOfBombs}</div>
+
+            <div className="score-result-icon">💣</div>
+            <div className="score-result-label">Number Of Columns</div>
+            <div className="score-result-value">{props.result.numberOfColumns}</div>
+
+            <div className="score-result-icon">💣</div>
+            <div className="score-result-label">Number Of Rows</div>
+            <div className="score-result-value">{props.result.numberOfRows}</div>
+
+            <div className="score-result-icon">💣</div>
+            <div className="score-result-label">Time</div>
+            <div className="score-result-value">{props.result.time}</div>
         </div>
     )
 }
-
 
 export default ScoreResult;
