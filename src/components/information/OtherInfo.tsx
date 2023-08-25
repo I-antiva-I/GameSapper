b20f426f5be304de7b4bfc2a86be14e5426820f9
I-antiva-I <a.a.ivanichenko@gmail.com>
2023-08-25 19:59:19 +0200

    Links to resources

--- src/components/information/OtherInfo.tsx
+++ src/components/information/OtherInfo.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import { GameResult } from "../../scripts/game_logic/game_process";
+import IconSVG from "../IconSVG";
+import { IconType } from "../../scripts/utility/managers/icon_manager";
 
 function OtherInfo()
 {   
     return(
         <div className="other-info">
-            <div>Logo</div>
-            <div>GitHub</div>
-            <div>Link</div>
+            <div className="other-info-item info-github">
+                <IconSVG iconType={IconType.GITHUB} iconColor="white"/>
+                <div className="info-link">
+                    <a href="https://github.com/I-antiva-I/GameSapper">GitHub - Game repository</a>
+                </div>
+            </div>
+            
+            <div className="other-info-item info-font-awesome">
+                <IconSVG iconType={IconType.FONT_AWESOME} iconColor="white"/>
+                <div className="info-link">
+                    <a href="https://fontawesome.com">FontAwesome - Icon library</a>
+                </div>
+            </div>
+
+            <div className="other-info-item info-wiki">
+                <IconSVG iconType={IconType.QUESTION_IN_CIRCLE} iconColor="white"/>
+                <div className="info-link">
+                    <a href="https://en.wikipedia.org/wiki/Minesweeper_(video_game)">Wikipedia - More about the game</a>
+                </div>
+            </div>
         </div>
     )
 }
